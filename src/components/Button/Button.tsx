7faece003de4d565fd4a75208184761fa011ef8b
Sid <siddhantchourasia@intugine.com>
2024-05-29 17:20:09 +0530

    setup fixes

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -17,147 +17,143 @@ export interface IButtonProps extends ButtonProps {
 	inputLabel?: boolean;
 	[key: string]: any;
 }
-const Button = forwardRef(
-	(
-		{
-			sx,
-			variant = "contained",
-			color = "primary",
-			children,
-			inputSize,
-			size = "small",
-			fromTextbox = false,
-			inputAlert,
-			text,
-			inputReq,
-			inputLabel,
-			disabled = false,
-			...otherProps
-		}: IButtonProps,
-		ref: ButtonProps["ref"]
-	) => {
-		const theme = useTheme();
+const Button = (
+	{
+		sx,
+		variant = "contained",
+		color = "primary",
+		children,
+		inputSize,
+		size = "small",
+		fromTextbox = false,
+		inputAlert,
+		text,
+		inputReq,
+		inputLabel,
+		disabled = false,
+		...otherProps
+	}: IButtonProps,
+	ref: ButtonProps["ref"]
+) => {
+	const theme = useTheme();
 
-		return (
-			<MuiButton
-				variant={variant}
-				color={color}
-				disabled={disabled}
-				size={size}
-				{...otherProps}
-				ref={ref}
-				sx={[
-					{
-						...(variant === "outlined" && {
-							color: `${
-								theme.palette[color === "secondary" ? "grey" : color][
-									color === "secondary" ? 350 : "main"
-								]
-							}`,
-							backgroundColor: `#ffffff`,
-							border: `1px solid`,
-							borderColor: `${
-								theme.palette[color === "secondary" ? "grey" : color][
-									color === "secondary" ? 350 : "main"
-								]
-							}`,
-							"&:hover": {
-								backgroundColor:
-									color !== "secondary"
-										? `${theme.palette[color].contrastText}`
-										: "inherit",
-							},
-							...(color === "secondary" && {
-								color: "#000000",
-								background: `#ffffff`,
-								"&:hover": {
-									borderColor: "#000000",
-								},
-							}),
-						}),
-						...(disabled && {
-							color: `${theme.palette.grey[100]} !important`,
+	return (
+		<MuiButton
+			variant={variant}
+			color={color}
+			disabled={disabled}
+			size={size}
+			{...otherProps}
+			ref={ref}
+			sx={[
+				{
+					...(variant === "outlined" && {
+						color: `${
+							theme.palette[color === "secondary" ? "grey" : color][
+								color === "secondary" ? 350 : "main"
+							]
+						}`,
+						backgroundColor: `#ffffff`,
+						border: `1px solid`,
+						borderColor: `${
+							theme.palette[color === "secondary" ? "grey" : color][
+								color === "secondary" ? 350 : "main"
+							]
+						}`,
+						"&:hover": {
 							backgroundColor:
-								variant === "contained"
-									? `${theme.palette.grey[150]} !important`
-									: "#FFFFFF",
-							borderColor:
-								variant === "outlined"
-									? `${theme.palette.grey[100]}`
+								color !== "secondary"
+									? `${theme.palette[color].contrastText}`
 									: "inherit",
-						}),
-						...(variant === "contained" && {
-							color: "#FFFFFF",
-							...(color === "secondary" && {
-								backgroundColor: theme.palette.grey[350],
-							}),
+						},
+						...(color === "secondary" && {
+							color: "#000000",
+							background: `#ffffff`,
 							"&:hover": {
-								background: `${
-									theme.palette[color === "secondary" ? "grey" : color][
-										color === "secondary" ? 650 : "dark"
-									]
-								}`,
+								borderColor: "#000000",
 							},
-							boxShadow: `none !important`,
 						}),
-						...(variant === "text" && {
-							fontWeight: 500,
-							...(size === "small" && {
-								...theme.typography.h6,
-							}),
-							...(size === "medium" && {
-								...theme.typography.h5,
-							}),
-							...(size === "large" && {
-								...theme.typography.h4,
-							}),
-							height: size === "small" ? "16px" : "20px",
+					}),
+					...(disabled && {
+						color: `${theme.palette.grey[100]} !important`,
+						backgroundColor:
+							variant === "contained"
+								? `${theme.palette.grey[150]} !important`
+								: "#FFFFFF",
+						borderColor:
+							variant === "outlined" ? `${theme.palette.grey[100]}` : "inherit",
+					}),
+					...(variant === "contained" && {
+						color: "#FFFFFF",
+						...(color === "secondary" && {
+							backgroundColor: theme.palette.grey[350],
+						}),
+						"&:hover": {
+							background: `${
+								theme.palette[color === "secondary" ? "grey" : color][
+									color === "secondary" ? 650 : "dark"
+								]
+							}`,
+						},
+						boxShadow: `none !important`,
+					}),
+					...(variant === "text" && {
+						fontWeight: 500,
+						...(size === "small" && {
+							...theme.typography.h6,
+						}),
+						...(size === "medium" && {
+							...theme.typography.h5,
+						}),
+						...(size === "large" && {
+							...theme.typography.h4,
+						}),
+						height: size === "small" ? "16px" : "20px",
+						minWidth: "50px !important",
+						padding: 0,
+						...(color === "secondary" && {
+							color: theme.palette.common.black,
+						}),
+						"&:hover": {
+							backgroundColor: "inherit",
+							color:
+								theme.palette[color === "secondary" ? "grey" : color][
+									color === "secondary" ? "650" : "dark"
+								],
+						},
+						...(fromTextbox && {
 							minWidth: "50px !important",
-							padding: 0,
-							...(color === "secondary" && {
-								color: theme.palette.common.black,
-							}),
-							"&:hover": {
-								backgroundColor: "inherit",
-								color:
-									theme.palette[color === "secondary" ? "grey" : color][
-										color === "secondary" ? "650" : "dark"
-									],
-							},
-							...(fromTextbox && {
-								minWidth: "50px !important",
-								position: `absolute`,
-								right: "12px",
-								height: inputSize === "small" ? "16px" : "20px",
-								top:
-									inputReq || inputLabel
-										? inputSize === "medium"
-											? "39px"
-											: "29px"
-										: inputSize === "medium"
+							position: `absolute`,
+							right: "12px",
+							height: inputSize === "small" ? "16px" : "20px",
+							top:
+								inputReq || inputLabel
+									? inputSize === "medium"
+										? "39px"
+										: "29px"
+									: inputSize === "medium"
 										? "11px"
 										: "9px",
-								color: disabled || inputAlert ? theme.palette.grey[100] : "",
-								padding: "8px 8px",
-								alignSelf: "center",
-								fontWeight: 500,
-								backgroundColor: "transparent",
-							}),
+							color: disabled || inputAlert ? theme.palette.grey[100] : "",
+							padding: "8px 8px",
+							alignSelf: "center",
+							fontWeight: 500,
+							backgroundColor: "transparent",
 						}),
-						"& .MuiButton-startIcon": {
-							justifyContent: "center",
-							marginLeft: 0,
-						},
+					}),
+					"& .MuiButton-startIcon": {
+						justifyContent: "center",
+						marginLeft: 0,
 					},
-					...(Boolean(sx) && Array.isArray(sx) ? sx : []),
-					Boolean(sx) && !Array.isArray(sx) ? sx : {},
-				]}
-			>
-				{text ? text : children}
-			</MuiButton>
-		);
-	}
-);
+				},
+				...(Boolean(sx) && Array.isArray(sx) ? sx : []),
+				Boolean(sx) && !Array.isArray(sx) ? sx : {},
+			]}
+		>
+			{text ? text : children}
+		</MuiButton>
+	);
+};
 
 Button.displayName = "Button";
 export default Button;
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { resolve } from "path";
+import { extname, relative, resolve } from "path";
 import dts from "vite-plugin-dts";
+
+import { glob } from "glob";
+import { fileURLToPath } from "node:url";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -17,6 +20,20 @@ export default defineConfig({
 		rollupOptions: {
 			// make sure to externalize deps that shouldn't be bundled
 			// into your library
+			input: Object.fromEntries(
+				glob.sync("src/**/*.{ts,tsx}").map((file) => [
+					// This remove `src/` as well as the file extension from each
+					// file, so e.g. src/nested/foo.js becomes nested/foo
+					relative("src", file.slice(0, file.length - extname(file).length)),
+					// This expands the relative paths to absolute paths, so e.g.
+					// src/nested/foo becomes /project/src/nested/foo.js
+					fileURLToPath(new URL(file, import.meta.url)),
+				])
+			),
+			output: {
+				assetFileNames: "assets/[name][extname]",
+				entryFileNames: "[name].js",
+			},
 			external: ["react, react/jsx-runtime"],
 		},
 	},
